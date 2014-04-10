3898c177eb8b7800fa68ccbab49234c353261d92
Mathieu Welche <surfinca4life@gmail.com>
2014-04-10 01:28:18 -0700

    updates to resume

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+/* functions baby */
+$(document).ready(function() {
+	/* $('.resume .skills li').draggable({
+		connectToSortable: "#skills-list",
+		containment: "parent",
+		helper: "clone",
+		revert: "invalid"
+	});
+	$('#skills-list').sortable({
+		revert: true
+	}); */
+	
+	/*
+	$('#skills-list').gridly({
+		base: 60, // px 
+		gutter: 20, // px
+		columns: 12
+	});
+	*/
+	$('.add-skill').hide();
+});
